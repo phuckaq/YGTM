a4549871f2aa4fd95b85965759d0881dd669e0ee
phuckaq <phuc.dev.vn@gmail.com>
2025-01-12 10:29:58 +0700

    starter

---
+++ src/main/java/phuc/ygtm/YgtmApplication.java
@@ -0,0 +1,13 @@
+package phuc.ygtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YgtmApplication.class, args);
+    }
+
+}
